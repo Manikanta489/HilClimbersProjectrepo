f65625b1a940074d8a182e80da29e04012d9588e
Manikantaltts123 <manikanta.suri@ltts.com>
2021-08-28 20:04:47 +0530

    final commit, MS

--- startup.m
+++ startup.m
@@ -6,10 +6,15 @@ close all
 % Archana (99005023)
 % Prasad (99005028)
 
+% BCM Features
+% LED Rear Light Module - Manikanta
+% Power Window Module - Archana 
+% HVAC Module - Prasad
+
 %% Setting paths 
 
 addpath('1_Model')
-addpath('1_Model\CustomLibrary\')
+addpath('1_Model\CustomLibrary/customFiles')
 addpath('6_PreviousModels')
 addpath('6_PreviousModels\Individual')
 addpath('6_PreviousModels\Integrated')
@@ -17,14 +22,14 @@ load('6_PreviousModels\Integrated\baseworkspace_variables.mat')
 
 
 %% Selection for Inputs - dashboards /MIL testing
-prompt = {'Enter = 0 for manual test, Enter = 1 for automated test'};
+prompt = {'Enter 0 for manual test, enter 1 for automated test'};
 dlgtitle='Input';
 dims=[1 35];
 definput={'0'};
 answer=inputdlg(prompt,dlgtitle,dims,definput);
 input_mode=str2double(answer{:});
 %% Selection of inputs for ECU subsystem - manual / SIL automated test
-prompt2 = {'Enter = 0 for manual test, Enter = 1 for sil automated test'};
+prompt2 = {'Enter 0 for manual test of reverse light subsystem, enter 1 for sil automated test on S function block'};
 dlgtitle2='Input';
 dims2=[1 35];
 definput2={'0'};
